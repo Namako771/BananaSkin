40a973f1c98be647e1ec99996ce00d6790bc151a
Namako <100671012+Namako771@users.noreply.github.com>
2022-07-29 23:51:31 +0900

    Initial commit

---
+++ src/main/java/dev/namako/bananaskin/BananaSkinClient.java
@@ -0,0 +1,13 @@
+package dev.namako.bananaskin;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class BananaSkinClient implements ClientModInitializer {
+  @Override
+  public void onInitializeClient() {
+
+  }
+}
---
+++ src/main/java/dev/namako/bananaskin/mixin/InGameHudMixin.java
@@ -0,0 +1,56 @@
+package dev.namako.bananaskin.mixin;
+
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public abstract class InGameHudMixin extends DrawableHelper {
+  private MatrixStack matrixStack;
+  private int startX;
+  private int startY;
+  private int count;
+  private boolean half;
+
+  @Shadow private int scaledWidth;
+
+  @Inject(method = "renderHealthBar", at = @At("HEAD"))
+  public void onDrawHeart(MatrixStack matrices, PlayerEntity p, int x, int y, int lines, int index, float max, int last, int health, int absorption, boolean blinking, CallbackInfo ci) {
+    matrixStack = matrices;
+    startX = scaledWidth / 2 + 10 + 9 * 8;
+    startY = y - 10;
+
+    float saturation = p.getHungerManager().getSaturationLevel();
+    count = Math.round(saturation) / 2;
+    half = Math.round(saturation) % 2 == 1;
+
+    drawFrame();
+    drawInside();
+  }
+
+  private void drawFrame() {
+    int i;
+    for(i=0;i<count;i++) {
+      drawTexture(matrixStack, startX + i * -8, startY , 124, 27, 9,9);
+    }
+    if(half) {
+      drawTexture(matrixStack, startX + i * -8, startY , 124, 27, 9,9);
+    }
+  }
+
+  private void drawInside() {
+    int i;
+    for(i=0;i<count;i++) {
+      drawTexture(matrixStack, startX + i * -8, startY , 52, 27, 9,9);
+    }
+    if(half) {
+      drawTexture(matrixStack, startX + i * -8, startY , 61, 27, 9,9);
+    }
+  }
+}

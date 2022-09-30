31c945d754728d126a87f2da4074f8a790074b5f
Namako <100671012+Namako771@users.noreply.github.com>
2022-09-30 22:39:23 +0900

    BAKA FIX

--- src/main/java/dev/namako/bananaskin/mixin/InGameHudMixin.java
+++ src/main/java/dev/namako/bananaskin/mixin/InGameHudMixin.java
@@ -12,34 +12,25 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin extends DrawableHelper {
-  private MatrixStack matrixStack;
-  private int startX;
-  private int startY;
-  private int count;
-  private boolean half;
 
   @Shadow private int scaledWidth;
 
   @Inject(method = "renderHealthBar", at = @At("HEAD"))
-  public void onDrawHeart(MatrixStack matrices, PlayerEntity p, int x, int y, int lines, int index, float max, int last, int health, int absorption, boolean blinking, CallbackInfo ci) {
-    matrixStack = matrices;
-    startX = scaledWidth / 2 + 10 + 9 * 8;
-    startY = y - 10;
+  public void onRenderHealthBar(MatrixStack matrices, PlayerEntity p, int x, int y, int lines, int index, float max, int last, int health, int absorption, boolean blinking, CallbackInfo ci) {
+    int startX = scaledWidth / 2 + 10 + 9 * 8;
+    int startY = y - 10;
     float saturation = p.getHungerManager().getSaturationLevel();
-    count = Math.round(saturation) / 2;
-    half = Math.round(saturation) % 2 == 1;
-    drawFood();
-  }
+    int count = Math.round(saturation) / 2;
+    boolean half = Math.round(saturation) % 2 == 1;
 
-  private void drawFood() {
     int i;
-    for(i=0;i<count;i++) {
-      drawTexture(matrixStack, startX + i * -8, startY , 124, 27, 9,9);
-      drawTexture(matrixStack, startX + i * -8, startY , 52, 27, 9,9);
+    for(i=0; i< count; i++) {
+      drawTexture(matrices, startX + i * -8, startY, 124, 27, 9,9);
+      drawTexture(matrices, startX + i * -8, startY, 52, 27, 9,9);
     }
     if(half) {
-      drawTexture(matrixStack, startX + i * -8, startY , 124, 27, 9,9);
-      drawTexture(matrixStack, startX + i * -8, startY , 61, 27, 9,9);
+      drawTexture(matrices, startX + i * -8, startY, 124, 27, 9,9);
+      drawTexture(matrices, startX + i * -8, startY, 61, 27, 9,9);
     }
   }
 }
